38c37cc15d44a133c44dd5a5f56a4532464b6ac9
Maha-I-Hendawy <maha.i.hendawy@gmail.com>
2024-09-13 20:00:59 +0300

    added somethings

--- text.py
+++ text.py
@@ -7,10 +7,12 @@ class Text:
 
 	def words(self):
 		word = [w.replace('.', '') for w in self.text.split(' ')]
-		
+		print(word)
 
 
 	def sentences(self):
 		sentence = [s.replace('.', '') for s in self.text.split('.')]
-		
+		print(sentence)
+
+
 
